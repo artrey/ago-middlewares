722e00a71610dfbf7ec12431b99b59a4b6c71ebf
Alexander Ivanov <oz.sasha.ivanov@gmail.com>
2021-03-18 00:49:20 +0300

    Fixed tests for identificator

--- pkg/middleware/identificator/identificator_test.go
+++ pkg/middleware/identificator/identificator_test.go
@@ -16,6 +16,10 @@ func TestIdentificatorHTTPMux(t *testing.T) {
 			func(writer http.ResponseWriter, request *http.Request) {
 				identifier, err := Identifier(request.Context())
 				if err != nil {
+					if err == ErrNoIdentifier {
+						writer.WriteHeader(http.StatusUnauthorized)
+						return
+					}
 					t.Fatal(err)
 				}
 				_, err = writer.Write([]byte(*identifier))
@@ -32,12 +36,16 @@ func TestIdentificatorHTTPMux(t *testing.T) {
 	}
 
 	tests := []struct {
-		name string
-		args args
-		want []byte
+		name     string
+		args     args
+		wantCode int
+		wantBody []byte
 	}{
-		{name: "GET", args: args{method: "GET", path: "/get", addr: "192.0.2.1:12345"}, want: []byte("192.0.2.1")},
-		// TODO: write for other methods
+		{name: "GET", args: args{method: "GET", path: "/get", addr: "192.0.2.1:12345"}, wantCode: 200, wantBody: []byte("192.0.2.1")},
+		{name: "GET2", args: args{method: "GET", path: "/get", addr: "127.0.0.1:666"}, wantCode: 200, wantBody: []byte("127.0.0.1")},
+		{name: "GET3", args: args{method: "GET", path: "/get", addr: "127.0.0.1"}, wantCode: 401, wantBody: []byte{}},
+		{name: "POST", args: args{method: "POST", path: "/get", addr: "192.0.2.1:12345"}, wantCode: 200, wantBody: []byte("192.0.2.1")},
+		{name: "POST2", args: args{method: "POST", path: "/post", addr: "192.0.2.1:12345"}, wantCode: 404, wantBody: []byte("404 page not found\n")},
 	}
 
 	for _, tt := range tests {
@@ -46,9 +54,13 @@ func TestIdentificatorHTTPMux(t *testing.T) {
 
 		response := httptest.NewRecorder()
 		mux.ServeHTTP(response, request)
-		got := response.Body.Bytes()
-		if !bytes.Equal(tt.want, got) {
-			t.Errorf("got %s, want %s", got, tt.want)
+		gotCode := response.Code
+		if tt.wantCode != gotCode {
+			t.Errorf("%s: got %d, wantCode %d", tt.name, gotCode, tt.wantCode)
+		}
+		gotBytes := response.Body.Bytes()
+		if !bytes.Equal(tt.wantBody, gotBytes) {
+			t.Errorf("%s: got %s, wantBody %s", tt.name, gotBytes, tt.wantBody)
 		}
 	}
 }
@@ -61,6 +73,10 @@ func TestIdentificatorChi(t *testing.T) {
 		func(writer http.ResponseWriter, request *http.Request) {
 			identifier, err := Identifier(request.Context())
 			if err != nil {
+				if err == ErrNoIdentifier {
+					writer.WriteHeader(http.StatusUnauthorized)
+					return
+				}
 				t.Fatal(err)
 			}
 			_, err = writer.Write([]byte(*identifier))
@@ -77,12 +93,16 @@ func TestIdentificatorChi(t *testing.T) {
 	}
 
 	tests := []struct {
-		name string
-		args args
-		want []byte
+		name     string
+		args     args
+		wantCode int
+		wantBody []byte
 	}{
-		{name: "GET", args: args{method: "GET", path: "/get", addr: "192.0.2.1:12345"}, want: []byte("192.0.2.1")},
-		// TODO: write for other methods
+		{name: "GET", args: args{method: "GET", path: "/get", addr: "192.0.2.1:12345"}, wantCode: 200, wantBody: []byte("192.0.2.1")},
+		{name: "GET2", args: args{method: "GET", path: "/get", addr: "127.0.0.1:666"}, wantCode: 200, wantBody: []byte("127.0.0.1")},
+		{name: "GET3", args: args{method: "GET", path: "/get", addr: "127.0.0.1"}, wantCode: 401, wantBody: []byte{}},
+		{name: "POST", args: args{method: "POST", path: "/get", addr: "192.0.2.1:12345"}, wantCode: 405, wantBody: []byte{}},
+		{name: "POST2", args: args{method: "POST", path: "/post", addr: "192.0.2.1:12345"}, wantCode: 404, wantBody: []byte("404 page not found\n")},
 	}
 
 	for _, tt := range tests {
@@ -91,9 +111,13 @@ func TestIdentificatorChi(t *testing.T) {
 
 		response := httptest.NewRecorder()
 		router.ServeHTTP(response, request)
-		got := response.Body.Bytes()
-		if !bytes.Equal(tt.want, got) {
-			t.Errorf("%s: got %s, want %s", tt.name, got, tt.want)
+		gotCode := response.Code
+		if tt.wantCode != gotCode {
+			t.Errorf("%s: got %d, wantCode %d", tt.name, gotCode, tt.wantCode)
+		}
+		gotBytes := response.Body.Bytes()
+		if !bytes.Equal(tt.wantBody, gotBytes) {
+			t.Errorf("%s: got %s, wantBody %s", tt.name, gotBytes, tt.wantBody)
 		}
 	}
 }
